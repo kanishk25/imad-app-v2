9af87f55f8d022a341e46d810ccd98b0205214e4
kanishk25 <kanishk25@gmail.com>
2017-03-15 18:54:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,26 @@
 // counter code
 var button = document.getElementById("counter");
-var counter = 0;
 
 button.onclick = function(){
     
-    counter = counter + 1;
+    // create the request
+    var request = new XMLHttpRequest();
     
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            // take some action
+            
+            if(request.status == 200){
+                var counter = request.respinseText ;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+        // if not done
+    };
     
+    // make the request
+    request.open('GET','http://kanishk25.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
