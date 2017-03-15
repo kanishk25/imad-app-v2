eaa4e1c440ba7ec2ff93ae2eb3c52968d5100d58
kanishk25 <kanishk25@gmail.com>
2017-03-15 19:20:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ submit.onclick = function(){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
-    ul.innerHtml = list;
+    ul.innerHTML = list;
 };
 
 
